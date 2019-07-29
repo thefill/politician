a39ede500179cf724e831f8fa85346799d3470cf
Filip Dabrowski <fifofil@gmail.com>
2019-07-29 16:27:41 +0100

    Changes

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,6 +1,7 @@
 import {Politician} from '../../src';
 import {availableServices} from './mocked-services';
 
+// TODO: move to cli bin
 // Create new app with server spawning automatically
 // and pass object containing service objects that should be mocked
 const app = new Politician({
--- example/src/mocked-services/index.ts
+++ example/src/mocked-services/index.ts
@@ -3,6 +3,7 @@ import {BaseRequestHandler} from '../../../src/classes/base-request-handler';
 import {StoreService} from '../../../src/services/store';
 import {SomeService} from './some-service';
 
+// TODO: move to cli bin
 /**
  * List of available services
  * @type {{SomeService: SomeService}}
